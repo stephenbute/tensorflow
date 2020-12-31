b15127130719b6c4b6916e8d848246c022d5dc7b
rui <rui@UCD-Birdo.local>
2020-12-31 17:00:56 +0800

    init

---
+++ lesson1.py
@@ -0,0 +1,28 @@
+
+import urllib.request
+import ssl
+import tensorflow as tf
+
+context = ssl._create_unverified_context()
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+x_train.shape()
+y_train.shape()
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=3)
+model.evaluate(x_test, y_test)
